86a50453111a7cf34631b4fc94518664f973aa76
WildWay <wild_way@list.ru>
2019-02-06 01:04:17 +0300

    Добавлено создание GPT и 5 партиций.

--- create_raid.sh
+++ create_raid.sh
@@ -6,23 +6,33 @@ mdadm --zero-superblock --force /dev/sd{b..g}
 # Создать RAID /dev/md0 с комбинированным уровнем 10 (1+0) из 6 дисков, с /dev/sdb по /dev/sdg
 mdadm -C /dev/md0 -l 10 -n 6 /dev/sd{b..g}
 
-# Создать файловую систему ext4 на RAID /dev/md0
-mkfs.ext4 /dev/md0
-
 # Создать конфигурационный файл, содержаший информацию о использующихся RAID и их компонентах.
 echo "DEVICE partitions" > /etc/mdadm.conf
 mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm.conf
 
-# Создать директорию /mnt/raid10
-mkdir /mnt/raid10
+# Создать таблицы разделов GPT на RAID /dev/md0
+parted -s /dev/md0 mklabel gpt
 
-# Добавить информацию о монтировании RAID - /dev/md0
-# точка монтирования                      - /mnt/raid10
-# файловая система                        - ext4 
-# опции монтирования                      - defaults
-# опция создания дампа файловой системы   - 1
-# опция порядка проверки файловой системы - 2
-echo -e "UUID=`blkid | grep /dev/md0 | cut -f 2 -d '"'`\t/mnt/raid10\text4\tdefaults\t1\t2" >> /etc/fstab
+# Создать директории для разделов
+mkdir -p /mnt/raid10/p{1..5}
+
+# Для каждого из 5 разделов
+for i in $(seq 1 5); do
+	# Создать раздел
+	parted /dev/md0 mkpart primary ext4 0% 20%
+	
+	# Создать файловую ext4 систему на разделе
+	mkfs.ext4 /dev/md0p$i
+	
+	# Добавить информацию о монтировании RAID - /dev/md0$i
+	# точка монтирования                      - /mnt/raid10/p$i
+	# файловая система                        - ext4 
+	# опции монтирования                      - defaults
+	# опция создания дампа файловой системы   - 1
+	# опция порядка проверки файловой системы - 2
+	echo -e "UUID=`blkid | grep /dev/md0p$i | cut -f 2 -d '"'`\t/mnt/raid10/p$i\text4\tdefaults\t1\t2" >> /etc/fstab		
+done
+
 
 # Примонтировать все устройства, описанные в /etc/fstab
 mount -a
