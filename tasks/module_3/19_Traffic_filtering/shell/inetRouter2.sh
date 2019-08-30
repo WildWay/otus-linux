6e2a085b2f64891bcbd95db6abd459ad0f5dcc96
WildWay <may-int@ya.ru>
2019-08-30 19:03:32 +0300

    Задание 19 обновлено (не завершено)

--- tasks/module_3/19_Traffic_filtering/shell/inetRouter2.sh
+++ tasks/module_3/19_Traffic_filtering/shell/inetRouter2.sh
@@ -9,11 +9,13 @@ echo '192.168.255.0/30 via 192.168.254.2
 ' > /etc/sysconfig/network-scripts/route-eth1
 
 iptables --flush && iptables --flush -t nat
+
 iptables -t nat -A PREROUTING -d 192.168.0.10 -p tcp -m tcp --dport 8080 -j DNAT --to-destination 192.168.250.2:80
 iptables -t nat -A POSTROUTING -d 192.168.250.2 -p tcp -m tcp --dport 80 -j SNAT --to-source 192.168.254.1
 iptables -t nat -A OUTPUT -d 192.168.0.10 -p tcp -m tcp --dport 80 -j DNAT --to-destination 192.168.250.2
 iptables -I FORWARD 1 -i eth2 -o eth1 -d 192.168.250.2 -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+
 iptables -t nat -A POSTROUTING -o eth0 -s 192.168.250.0/28 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth0 -s 192.168.254.0/30 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth0 -s 192.168.255.0/30 -j MASQUERADE
